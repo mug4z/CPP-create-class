51eb086d4c882e2d7cdebc0fc7973281e66a8dc8
Timothee Pierre-Andre Frily <tfrily@c1r10s4.42lausanne.ch>
2024-12-19 13:18:17 +0100

    Change the PATH
    Change the PATH of the template so that a git clone is enough
    
    Add the template_cpp_path and template_hpp_path variable.

--- class_creator.sh
+++ class_creator.sh
@@ -9,18 +9,20 @@ if [ $# -eq 1 ]; then
 	exit
 fi
 
+template_cpp_path="./CPP-create-class/CPP_TEMPLATE/class_template_cpp"
+template_hpp_path="./CPP-create-class/CPP_TEMPLATE/class_template_hpp"
+class_name_uppercase=$(echo "$var" | tr '[:lower:]' '[:upper:]')
 
 for var in "$@"
 do
-	class_name_uppercase=$(echo "$var" | tr '[:lower:]' '[:upper:]')
 	if [ -f "./$var.cpp" ] || [ -f "./src/$var.cpp" ] ; then
 		echo "File $var.cpp already exist"
 	else
 		if [ -d "./src" ];then
-			cat "$HOME"/CPP_TEMPLATE/class_template_cpp > ./src/"$var".cpp
+			cat $template_cpp_path > ./src/"$var".cpp
 			sed -i -e "s/{CLASS}/$var/g" ./src/"$var".cpp
 		else
-			cat "$HOME"/CPP_TEMPLATE/class_template_cpp > ./"$var".cpp
+			cat  $template_cpp_path > ./"$var".cpp
 			sed -i -e "s/{CLASS}/$var/g" ./"$var".cpp
 		fi
 	fi
@@ -29,12 +31,12 @@ do
 		echo "File $var.hpp already exist"
 	else
 		if [ -d "./inc" ];then
-			cat "$HOME"/CPP_TEMPLATE/class_template_hpp> ./inc/"$var".hpp
+			cat $template_hpp_path > ./inc/"$var".hpp
 			sed -i -e "s/#ifndef {CLASS}/#ifndef $class_name_uppercase/g" ./inc/"$var".hpp
 			sed -i -e "s/#define {CLASS}/#define $class_name_uppercase/g" ./inc/"$var".hpp
 			sed -i -e "s/{CLASS}/$var/g" ./inc/"$var".hpp
 		else
-			cat "$HOME"/CPP_TEMPLATE/class_template_hpp> ./"$var".hpp
+			cat $template_hpp_path > ./"$var".hpp
 			sed -i -e "s/#ifndef {CLASS}/#ifndef $class_name_uppercase/g" ./"$var".hpp
 			sed -i -e "s/#define {CLASS}/#define $class_name_uppercase/g" ./"$var".hpp
 			sed -i -e "s/{CLASS}/$var/g" ./"$var".hpp
